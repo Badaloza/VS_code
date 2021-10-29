04e4da0c2fd9383da2b24baaefe344f2a289ad33
Badaloza <83389535+Badaloza@users.noreply.github.com>
2021-10-29 23:21:04 +0530

    2.0

--- AL mouse.py
+++ AL mouse.py
@@ -4,7 +4,7 @@ import handmodule as htm
 import time
 import pyautogui
 
-#################################### hiiii
+####################################
 # wCam, hCam = pyautogui.size() # For full screen webCam
 wCam, hCam = 640, 480
 p1, p2 = int((wCam/640)*400), int((hCam/480)*100)
@@ -37,19 +37,16 @@ while True:
     if len(lmList) != 0:
         x1, y1 = lmList[8][1:]
         x2, y2 = lmList[12][1:]
-        x3, y3 = lmList[16][1:]
-
+        x3, y3 = lmList[16][1:] 
         cx1 = x1
         cx2 = x2
         cx3 = x3
         cy1 = y1
         cy2 = y2
-        cy3 = y3
-
+        cy3 = y3    
         # check which finger is up
         fingers = detector.fingersUp()
-        cv2.rectangle(img, (p1, p2), (p3, p4), (255, 0, 255), 2)
-
+        cv2.rectangle(img, (p1, p2), (p3, p4), (255, 0, 255), 2)    
         # only index finger: moving mode
         if fingers[1] == 1 and fingers[2] == 0 and fingers[3] == 0 and fingers[4] == 0 and fingers[0] == 1:
             # convert coordinates
@@ -57,36 +54,28 @@ while True:
             iny = np.interp(y1, (p2, p4), (0, hScr))
             # smoothening values
             clocX = plocX + (inx-plocX) / smoothening
-            clocY = plocY + (iny-plocY) / smoothening
-
+            clocY = plocY + (iny-plocY) / smoothening   
             # move mouse to x=clocX, y=clocY
             pyautogui.moveTo(x=clocX, y=clocY)
             # autopy.mouse.move(wScr-clocX, clocY)
             cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
-            plocX, plocY = clocX, clocY
-
-        # both index and middle finger up: clicking mode
+            plocX, plocY = clocX, clocY 
+        # both index and thumb up: clicking mode
         if fingers[1] == 1 and fingers[2] == 0 and fingers[3] == 0 and fingers[4] == 0 and fingers[0] == 0:
             # distance between finger
             length, img, lineInfo = detector.findDistance(4, 5, img, draw=False)
-
             # click if distance short
             if length > 50:
                 cv2.circle(img, (x1, y1), 15, (0, 255, 0), cv2.FILLED)
-                # cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
                 pyautogui.click(clicks=1, interval=0.3)
         # VOLUME CONTROL
         if fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 0 and fingers[4] == 0 and fingers[0] == 0:
-            
-            cv2.putText(img, str("Volume Control"), (200,100), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
-            # cv2.putText(img, str(py2), (10,150), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
-            # cv2.putText(img, str(cy2), (10,190), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
 
+            cv2.putText(img, str("Volume Control"), (200,100), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)   
             if (cy1+10) < py1 and cy2 < py2:
                 pyautogui.press("volumeup")
             if (cy1-10) > py1 and cy2 > py2:
                 pyautogui.press("volumedown")
-                # pyautogui.hotkey('alt', 'f4')
 
 
     # frame rate
@@ -105,4 +94,8 @@ while True:
     cv2.putText(img, str(int(fps)), (10,200), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
 
     cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    key = cv2.waitKey(2)
+    if key==ord('q') or key ==ord('Q'):
+        break
+cap.release()
+
